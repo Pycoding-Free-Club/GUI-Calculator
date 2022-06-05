2f3db4ba7802fd763b9d70088980b6f46b3bd0cf
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-06-06 01:23:19 +0900

    Import each Class and apply..

--- src/main.py
+++ src/main.py
@@ -2,58 +2,39 @@
 from tkinter import *
 from math import *
 
-class Calculator:
-  def __init__(self) -> None:
-    self.window_option = {
-      "width": 500,
-      "height": 756,
-      "title": "Scientific Calculator",
-      "font": ["*Font", "맑은고딕 25"],
-      "alpha": 0.95,
-    }
+from buttons import Buttons
+from windows import Windows
+from calculator import Calculator
 
-    self.window = Tk()
-    self.setup()
-
-  def setup(self):
-    self.window.geometry(
-      f"""{self.window_option.get("width")}x{self.window_option.get("height")}"""
-    )
-    self.window.title(self.window_option.get("title"))
-    print(self.window_option.get("font")[0])
-    self.window.option_add(self.window_option.get("font")[0], self.window_option.get("font")[1])
-    self.window.attributes("-alpha", self.window_option.get("alpha"))
-
+# Create Window and Calculator
+window = Windows()
 calculator = Calculator()
 
-calculator.window.mainloop();
+# Place the buttons pretty
+texts = [ "sin", "cos", "tan", "%", "π", "√", "!", "^", "7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "X", ".", "0", "=", "÷" ]
+btn_Pos = (0, 168)
+column_and_row = (6, 4)
+buttons = []
 
+text_idx = 0
+for i in range(column_and_row[0]):
+  for j in range(column_and_row[1]):
+    buttons.append(
+      Buttons(
+        window.window,
+        True,
+        [
+          btn_Pos[0]+(j*125),
+          btn_Pos[1]+(i*84)
+        ],
+        texts[text_idx]
+      )
+    )
 
-# BbuttonSize = (250, 84)
-# SbuttonSize = (125, 84)
-#
-# # Define the function which are help calculating and etc...
-# def place_btn(isSmall, x, y, text, handler):
-#     button = Button(win, text=text, background = "white", command=handler)
-#     if isSmall == True:
-#         button.place(x=x, y=y, width=SbuttonSize[0], height=SbuttonSize[1])
-#     else:
-#         button.place(x=x, y=y, width=BbuttonSize[0], height=BbuttonSize[1])
-#
-# def get_number(number):
-#     print(f"Button is clicked! And value of that is...{number}")
-#
-# texts = [ "sin", "cos", "tan", "%", "π", "√", "!", "^", "7", "8","9","+","4","5","6","-","1","2","3", "X", ".", "0", "=", "÷" ]
-# btn_Pos = (0, 168)
-# column_and_row = (6, 4)
-#
-# text_idx = 0
-# for i in range(column_and_row[0]):
-#     for j in range(column_and_row[1]):
-#         place_btn(True, btn_Pos[0]+(j*125), btn_Pos[1]+(i*84), texts[text_idx], command=get_number)
-#         text_idx += 1
-#
-# place_btn(False, 0, 672, "AC", command=get_number)
-# place_btn(False, 250, 672, "Save", command=get_number)
-#
-# win.mainloop()
+    text_idx += 1
+
+buttons.append(Buttons(window.window, False, [0, 672], "Save"))
+buttons.append(Buttons(window.window, False, [250, 672], "AC"))
+
+# Start Calculator
+window.window.mainloop();
