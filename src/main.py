dec2211920d0fd5d69202f3e14282563e344201f
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-06-13 01:26:23 +0900

    Feat - Add Clean, Delete and Error function

--- src/main.py
+++ src/main.py
@@ -22,16 +22,16 @@ def place_btn(isSmall, x, y, text, command):
     else:
         button.place(x=x, y=y, width=BbuttonSize[0], height=BbuttonSize[1])
 
-def make_lambda(number):
-    return lambda: add_number(number)
-
 def add_number(value):
     global expression
 
     if value == "=":
-        result = eval(expression)
-        expression = str(result)
-        change_text(result)
+        try:
+            result = eval(expression)
+            expression = str(result)
+            change_text(result)
+        except:
+            change_text("Error")
 
     else:
         expression += value
@@ -40,6 +40,19 @@ def add_number(value):
 def change_text(value):
     global label
     label.config(text=value)
+
+def save():
+    pass
+
+def clean():
+    change_text("")
+
+def delete():
+    global label
+    label.config(text=label.cget("text")[0:-1])
+
+def make_lambda(number):
+    return lambda: add_number(number)
 
 texts = [ "sin", "cos", "tan", "%", "π", "√", "(", ")", "7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", ".", "0", "=", "/" ]
 btn_Pos = (0, 168)
@@ -51,8 +64,9 @@ for i in range(column_and_row[0]):
         place_btn(True, btn_Pos[0]+(j*125), btn_Pos[1]+(i*84), texts[text_idx], make_lambda(texts[text_idx])) 
         text_idx += 1
 
-place_btn(False, 0, 672, "AC", command=add_number)
-place_btn(False, 250, 672, "Save", command=add_number)
+place_btn(False, 0, 672, "AC", command=clean)
+place_btn(True, 250, 672, "Delete", command=delete)
+place_btn(True, 375, 672, "Save", command=save)
 
 label = Label(win, text="", fg="black", relief="solid")
 label.place(x=0, y=0, width=500, height=168)
